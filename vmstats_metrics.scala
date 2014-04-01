87d5db2bb197c64831163810f64d4e791f5efa1e
Bob Williams <bobwilliams.ii@gmail.com>
2014-03-31 20:04:35 -0400

    updating to make one event for all stats from vm_stat call

--- vmstats_metrics.scala
+++ vmstats_metrics.scala
@@ -1,20 +1,25 @@
 import scala.sys.process._
 import scala.io._
 import scala.util.parsing.json._
+import scala.collection.mutable.{Map => MMap}
 
 val vmstatCmd = s"vm_stat"
 val io = new ProcessIO(
 	_ => (),
 	stdout => {
 		val res = Source.fromInputStream(stdout).getLines.drop(1)
+		val event = MMap[String, Long]()
 		res.foreach(
 			x => {
 				val line = x.split(":( )+")
 				val key = line(0)
 				val value = line(1).stripSuffix(".").toLong
-				var json = JSONObject(Map("timestamp" -> System.currentTimeMillis, "data" -> JSONObject(Map(key -> value)))).toString()
-				val url = "http://localhost:8080/db/usagestats/vm_stats"
-				val curlCmd = Seq("curl", "-s", "-H", "'Content-Type: application/json'", "-X", "POST", "-d", s"${json}", url)
-				Process(curlCmd).run})},
+				event += (key -> value)
+			})
+		val url = "http://localhost:8080/db/usagestats/vm_stats"
+		var json = JSONObject(Map("timestamp" -> System.currentTimeMillis, "data" -> JSONObject(event.toMap))).toString()
+		val curlCmd = Seq("curl", "-s", "-H", "'Content-Type: application/json'", "-X", "POST", "-d", json, url)
+		Process(curlCmd).run
+	},
 	_ => ())
 val proc = Process(vmstatCmd).run(io)
